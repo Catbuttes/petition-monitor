244e87eb9f4363fe360f0755b93d25a2e8051c05
Azure Pipeline Build <catbuttes@outlook.com>
2019-09-19 10:45:25 +0000

    Build for Github Pages [skip ci]

--- docs/precache-manifest.3454aa52ec6e9fa3ec11a70e906cba87.js
+++ docs/precache-manifest.3454aa52ec6e9fa3ec11a70e906cba87.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/petition-monitor/static/js/runtime~main.b9a1e37c.js"
   },
   {
-    "revision": "b5821fe7102c63eddd16",
-    "url": "/petition-monitor/static/js/main.eda1faae.chunk.js"
+    "revision": "1e055e3cc4a6a73eb535",
+    "url": "/petition-monitor/static/js/main.4b038880.chunk.js"
   },
   {
     "revision": "9816434f66faf46d709a",
     "url": "/petition-monitor/static/js/2.ce9fd5c2.chunk.js"
   },
   {
-    "revision": "b5821fe7102c63eddd16",
+    "revision": "1e055e3cc4a6a73eb535",
     "url": "/petition-monitor/static/css/main.5bddd580.chunk.css"
   },
   {
-    "revision": "b5a79ac06db2f9e265e14ab0a1e3f3bf",
+    "revision": "a72465d222205a3cdff3a27e0e66b58b",
     "url": "/petition-monitor/index.html"
   }
 ];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/petition-monitor/precache-manifest.d02252bb35d8e6522302d2da934fc37c.js"
+  "/petition-monitor/precache-manifest.3454aa52ec6e9fa3ec11a70e906cba87.js"
 );
 
 workbox.clientsClaim();
